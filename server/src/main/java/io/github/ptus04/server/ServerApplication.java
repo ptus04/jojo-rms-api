7c92c2656ce3aff9d1030557330841ebd2af5286
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-03-01 12:57:53 +0700

    Initial Commit

---
+++ server/src/main/java/io/github/ptus04/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package io.github.ptus04.server;
+
+import com.azure.spring.cloud.autoconfigure.implementation.storage.queue.AzureStorageQueueAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = AzureStorageQueueAutoConfiguration.class)
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/io/github/ptus04/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ptus04.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
